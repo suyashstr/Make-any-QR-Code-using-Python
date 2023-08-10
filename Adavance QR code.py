f706d0d6861bc3bb1a7f767f5147166634947035
suyashstr <141958076+suyashstr@users.noreply.github.com>
2023-08-11 01:09:25 +0530

    Add files via upload

---
+++ Adavance QR code.py
@@ -0,0 +1,10 @@
+import qrcode
+from PIL import Image
+
+qr=qrcode.QRCode(version=1,
+                 error_correction=qrcode.constants.ERROR_CORRECT_H ,
+                 box_size=10, border=4,)
+qr.add_data("https://github.com/suyashftr")
+qr.make(fit=True)
+image=qr.make_image(fill_color="blue",background_color="white")
+image.save("linkedIn_colourful.png")
---
+++ QR code.py
@@ -0,0 +1,3 @@
+import qrcode
+image=qrcode.make("https://github.com/suyashftr")
+image.save("Github.png")
